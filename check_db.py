5c4fa460f145cac5f957b6e0543fb408471e9251
Manya Sethi <manyasethi777@gmail.com>
2026-04-29 19:45:19 +0530

    Update check_db.py

--- check_db.py
+++ check_db.py
@@ -1,5 +1,16 @@
-import psycopg2
-conn = psycopg2.connect(host='localhost', database='expense_db', user='postgres', password='agrim2510')
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='localhost',
+    database='expense_tracker',
+    user='root',        # change if needed
+    password='',        # change if needed
+    port=3306           # MAMP default is 8889, XAMPP is 3306
+)
+
 cur = conn.cursor()
-cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name ILIKE 'categories'")
+cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'categories' AND TABLE_SCHEMA = 'expense_tracker'")
 print(cur.fetchall())
+
+cur.close()
+conn.close()
